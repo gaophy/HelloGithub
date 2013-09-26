cace725f291e69e3e84c85594381e55c2618cac7
gaohui <gaohui@qq.com>
2013-09-26 15:32:49 +0800

    2commit

---
+++ src/HelloGithub.java
@@ -0,0 +1,21 @@
+/*
+ * @(#)HelloGithub.java
+ * Copyright (C) 2003-2013 www.dsideal.com All rights reserved
+ */
+
+/**
+ * @author <b>gaohui</b>, Copyright &#169; 2003
+ * @version 1.0, 2013-9-26
+ *
+ */
+public class HelloGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2222222222");
+	}
+
+}
